3672f62daa49d37865e9ae7d9bfb8bc9afca52d7
Lucas <lucas.tallos@tallos.com.br>
2022-09-27 10:56:07 -0300

    feat: Finalizando tabela de usuarios

---
+++ src/components/Modals/NewUser.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-modal
+      id="modal-login"
+      :visible="isAddNewUserSidebarActive"
+     
+      hide-footer
+      hide-header-close
+      header-text-variant="light"
+      header-bg-variant="dark"
+      title="Editar Funcionário"
+    >
+      <b-form>
+        <b-form-group class="pb-3 InputPosition">
+          <p class="sub">Nome</p>
+          <b-input
+            type="text"
+            class="inputEdit shadow-none"
+            v-model="conteudotable.name"
+          />
+        </b-form-group>
+        <b-form-group class="pb-3 InputPosition">
+          <p class="sub">Email</p>
+          <b-input
+            type="email"
+            id="email"
+            class="inputEdit shadow-none"
+            v-model="conteudotable.email"
+          />
+        </b-form-group>
+        <b-form-group class="pb-3 InputPosition">
+          <p class="sub">Senha</p>
+          <b-input
+            type="password"
+            class="inputEdit shadow-none"
+            v-model="conteudotable.password"
+          />
+        </b-form-group>
+        <div class="d-flex justify-content-between btn-size">
+          <b-button @click.prevent="Editar()" variant="outline-success">
+            Atualizar
+          </b-button>
+          <b-button @click.prevent="hideModal()" variant="outline-danger">
+            Cancelar
+          </b-button>
+        </div>
+      </b-form>
+    </b-modal>
+  </div>
+</template>
+<script>
+import { VBModal } from 'bootstrap-vue';
+export default {
+
+};
+</script>
